4f72b8c08a47e7e30de0d64ddccf99d28453f10d
Hemant <dubeyhemantjpr@gmail.com>
2024-04-13 00:02:21 +0530

    created website

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -5,19 +5,19 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSSAGE_SENDER_ID;
-const appId = process.env.REACT_APP_FIREBASE_APP_ID;
-const measurementId = process.env.REACT_APP_FIREBASE_MEASURMENT_ID;
+const apiKey = "REACT_APP_FIREBASE_API_KEY =AIzaSyD3Si0bgqNZplqnmjDWabwE6naCIcKMY5Q";
+const authDomain = "my-first-project-198dd.firebaseapp.com";
+const messagingSenderId = "7653756341";
+const appId = "1:7653756341:web:42031799111549ed02ddfc";
+const measurementId =  'measurementId';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: apiKey ,
   authDomain: authDomain,
-  projectId: "authenticationpractice-9fc3b",
-  storageBucket: "authenticationpractice-9fc3b.appspot.com",
+  projectId: "my-first-project-198dd",
+  storageBucket: "my-first-project-198dd.appspot.com",
   messagingSenderId: messagingSenderId,
   appId: appId,
   measurementId: measurementId
